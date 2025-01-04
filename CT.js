e03f3f93f8db822de42ed56aa0f2937e771f8679
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-01-04 19:59:09 +0530

    configuration of countdown timer

--- CT.js
+++ CT.js
@@ -1,25 +1,121 @@
-// let countdown = document.getElementById("ctd");
-// let button = document.getElementById("but");
-// let screen = document.getElementById("scr");
 
 
-// button.addEventListener("click", () => {
-//     console.log(countdown.value);
-//     let i = countdown.value;
-//     while (i >= 0) {
-//         screen.textContent = i;
-//         console.log(i);
-//         i -= 1
-//     }
-// })
 
-let count = 0;
+let start = document.getElementById("start");
+let screen = document.getElementById("screen");
+let alarm = document.getElementById("myaudio");
 
-const intervalID = setInterval(() => {
-    console.log(`Count: ${count}`);
-    count++;
 
-    if (count > 5) {  // Stop after 5 counts
-        clearInterval(intervalID);
+
+let hours = document.getElementById("count");
+let minutes = document.getElementById("minute");
+let seconds = document.getElementById("second");
+screen.textContent = `00:00:00`;
+
+
+start.addEventListener("click", () => {
+
+
+    let t1 = parseInt(hours.value, 10) || 0;
+    let t2 = parseInt(minutes.value, 10) || 0;
+    let t3 = parseInt(seconds.value, 10) || 0;
+    console.log(t1);
+    console.log(t2);
+    console.log(t3);
+
+    let ct = setInterval(() => {
+        screen.textContent = `${t1}:${t2}:${t3}`;
+        if (t3 > 0) {
+            t3--;
+        } else if (t2 > 0) {
+            t2--;
+            t3 = 59;
+        } else if (t1 > 0) {
+            t--;
+            t2 = 59;
+            t3 = 59
+        } else {
+            clearInterval(ct);
+            // window.location.reload();
+            console.log("done");
+            alarm.play();
+        }
+
+    }, 1000)
+
+})
+
+
+
+let rain_audio = document.getElementById("rain_aud");
+let nature_audio = document.getElementById("bird_aud");
+
+let rain_icon = document.getElementById("rain_icon");
+let nature_icon = document.getElementById("bird_icon");
+
+let water_icon = document.getElementById("water_icon");
+let water_audio = document.getElementById("water_audio");
+
+let wind_icon = document.getElementById("wind_icon");
+let wind_audio = document.getElementById("wind_audio");
+
+let tree_icon = document.getElementById("tree_icon");
+let tree_audio = document.getElementById("tree_audio");
+
+
+let audio = true;
+rain_icon.addEventListener("click", () => {
+    if (audio) {
+        rain_audio.play();
+        audio = false;
+    } else {
+        rain_audio.pause();
+        audio = true;
+        audio.currentTime = 0
     }
-}, 1000);  // Interval of 1000 ms (1 second)
+})
+
+nature_icon.addEventListener("click", () => {
+    if (audio) {
+        nature_audio.play();
+        audio = false;
+    } else {
+        nature_audio.pause();
+        audio = true;
+        audio.currentTime = 0;
+    }
+})
+
+water_icon.addEventListener("click", () => {
+    if (audio) {
+        water_audio.play();
+        audio = false;
+    } else {
+        water_audio.pause();
+        audio = true;
+        audio.currentTime = 0;
+    }
+})
+
+wind_icon.addEventListener("click", () => {
+    if (audio) {
+        wind_audio.play();
+        audio = false;
+    } else {
+        wind_audio.pause();
+        audio = true;
+        audio.currentTime = 0;
+    }
+})
+
+tree_icon.addEventListener("click", () => {
+    if (audio) {
+        tree_audio.play();
+        audio = false;
+    } else {
+        tree_audio.pause();
+        audio = true;
+        audio.currentTime = 0;
+    }
+})
+
